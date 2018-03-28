69ea3a58b6623c03d06e14775f3f0e7aacd2637e
Anoop Jain <anoopj08@gmail.com>
2018-03-27 23:21:52 -0400

    figuring out how to make it fit

--- nb.py
+++ nb.py
@@ -6,9 +6,39 @@ from sys import argv
 
 def main():
     trainingFile = argv[1]
-    trainingData = pd.read_csv(trainingFile, sep=',', quotechar='"', header=None, engine='python')
-    X = lumpy.asarray(trainingData.as_matrix())
-    Y = lumpy.asarray()[0,1,2,3,4])
+    trainingData = pd.read_csv(trainingFile, sep=',', quotechar='"', header=0, engine='python')
+    X = trainingData.as_matrix()
+
+
+    #print(X[0])
+    #Y = lumpy.asarray([0,1,2,3,4])
+    max = 0
+    for dp in X:
+        currList = dp[0].split()
+        if len(currList) > max:
+            max = len(currList)
+        dp[0] = currList
+
+    for dp in X:
+        currList = dp[0]
+        size = len(currList)
+        for i in range(max-size):
+            lumpy.append(currList, '0', axis=None)
+        dp[0] = currList
+    #print(X)
+    param1 = X[:,0]
+    param2 = X[:,1]
+    #print(param1.tolist())
+    #exit()
+    #print(param2.tolist())
+    #print(len(param1[0]), len(param1[1]),len(param1),len(param2))
+    clf = MultinomialNB()
+    # lumpy.array(list(param2), dtype=int)
+    param2.reshape(1,-1)
+    print(param1[0],param2[0])
+    #print(type(param2))
+    clf.fit(param1,param2)
+    print(clf.predict(["Everybody is dead"]))
 
 
 
